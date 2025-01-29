94ac306243dab3fcc439f89da1a55e3cee69fce4
johnpaul <dexcripter@gmail.com>
2025-01-29 21:40:58 +0100

    routing updating, getting, and deleting controllers for items

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { createItem } from "../controllers/items.controller";
+import {
+  createItem,
+  deleteItem,
+  getItems,
+  updateItem,
+} from "../controllers/items.controller";
 import upload from "../services/multer";
 
 const Router = express.Router();
 
-Router.route("/").post(upload.single("image"), createItem);
+Router.route("/").post(upload.single("image"), createItem).get(getItems);
+Router.route("/:id")
+  .delete(deleteItem)
+  .patch(upload.single("image"), updateItem);
 
 export { Router as itemsRouter };
